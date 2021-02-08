2542f6a94f97a9c22637e35e12589d99fd1c4fb2
philenal <philena.liu@gmail.com>
2021-02-08 03:04:47 -0600

    look into symlink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './utilities.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/pages/Home.js";
+import About from "./components/pages/About.js";
+import Resume from "./components/pages/Resume.js";
+import Sketches from "./components/pages/Sketches.js";
+import Projects from "./components/pages/Projects.js";
+import MyNavBar from "./components/modules/MyNavBar.js";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyNavBar/>
+        <div id="home">
+          <Home/>
+        </div>
+        <div id="projects">
+          <Projects/>
+        </div>
+        <div id="sketches">
+          <Sketches/>
+        </div>
+        <div id="about">
+          <About/>
+        </div>
+        <div id="resume">
+          <Resume/>
+        </div>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
