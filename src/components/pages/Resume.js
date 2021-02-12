dba3d370e2f21f3a2b80c7bccd33bab4021453e7
philenal <philena.liu@gmail.com>
2021-02-12 15:54:10 -0600

    added new pics

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Resume extends Component {
+    render() {
+        return (
+            <div className="Resume-page">
+               heyo
+               
+            </div>
+
+        );
+    }
+}
+
+export default Resume;
